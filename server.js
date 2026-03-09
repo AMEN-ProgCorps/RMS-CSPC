175916dd5ed64170c5a56163a0ba5152235f5f25
MacCloud <cameronmaccloud13@gmail.com>
2026-03-09 12:06:31 +0800

    layout for complete access

--- server.js
+++ server.js
@@ -1,15 +1,31 @@
 // Import the http module
-const http = require("http");
+const http = require("express");
+const port = 3000;
+const app = express();
 
-// Create a server
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/html");
-
-    res.end("Welcome to Node.js Tutorial");
+// First middleware
+app.use((req, res, next) => {
+  console.log('Middleware 1: This always runs');
+  next();
 });
 
+// Second middleware
+app.use((req, res, next) => {
+  console.log('Middleware 2: This also always runs');
+  next();
+});
+
+// Route handler
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/login.html');
+});
+
+app.get('/error', (req, res) => {
+  res.sendFile(__dirname + '/public/error.html');
+});
+
+
 // Listen on port 3000
-server.listen(3000, () => {
-    console.log("Server is running on http://localhost:3000");
+app.listen(port, () => {
+    console.log("Server is running on http://localhost:"+port);
 });

560edc5b5a347504dc597a5c8406cef9c389a6e6
Finnapple <johnlagriada@protonmail.com>
2026-07-19 20:57:20 +0800

    Bug Fixed

--- records-management-system/public/chatify/index.php
+++ records-management-system/public/chatify/index.php
@@ -2133,7 +2133,7 @@ $dark_mode = isset($_COOKIE['dark_mode']) && $_COOKIE['dark_mode'] === 'enabled'
       cursor: default;
     }
     .verified-badge::after {
-      content: 'Super Admin';
+      content: '';
       position: absolute;
       bottom: calc(100% + 4px);
       left: 50%;
@@ -2403,7 +2403,7 @@ $dark_mode = isset($_COOKIE['dark_mode']) && $_COOKIE['dark_mode'] === 'enabled'
                 <circle cx="12" cy="12" r="12" fill="#1b74e4"/>
                 <path d="M7 12.5l3.5 3.5 6.5-7" stroke="#fff" stroke-width="2.2" stroke-linecap="round" stroke-linejoin="round"/>
               </svg>
-              <span>Super Admin</span>
+              <span></span>
             </span>
             <?php endif; ?>
           </div>
@@ -3946,7 +3946,7 @@ $dark_mode = isset($_COOKIE['dark_mode']) && $_COOKIE['dark_mode'] === 'enabled'
     function injectBadge(el) {
       const badge = document.createElement('span');
       badge.className = 'verified-badge';
-      badge.title = 'Super Admin';
+      badge.title = '';
       badge.innerHTML = `<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
         <circle cx="12" cy="12" r="12" fill="#1b74e4"/>
         <path d="M7 12.5l3.5 3.5 6.5-7" stroke="#fff" stroke-width="2.2" stroke-linecap="round" stroke-linejoin="round"/>
